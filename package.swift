bb282deb4c8f748154c7413d063f8ac1e29d3edd
Victor Ewert <victor.ewert@ewert-technologies.ca>
2021-06-04 15:58:58 -0700

    Added Package.swift

---
+++ package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+  name: "SwiftyButton",
+  platforms: [
+    .iOS(.v9)
+  ],
+  products: [
+    .library(name: "SwiftyButton", targets: ["SwiftyButton"])
+  ],
+  targets: [
+    .target(
+      name: "SwiftyButton",
+      path: "SwiftyButton",
+    )
+  ]
+)
